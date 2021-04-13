48b76dabdd4c84e2839bda966f2b87b41fae4d2a
Tiago <fc52741@alunos.fc.ul.pt>
2021-04-13 10:29:22 +0100

    postservice done

--- frontend/src/app/wall/wall.component.ts
+++ frontend/src/app/wall/wall.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { PostService } from '../post.service';
+import { Post } from '../post';
 import { User } from '../user';
 import { UserService } from '../user.service';
 
@@ -11,8 +13,8 @@ import { UserService } from '../user.service';
 export class WallComponent implements OnInit {
   user: User | undefined;
   users: User[] = [];
-
-  constructor(private router: Router, private userService: UserService) { 
+  posts: Post[] = [];
+  constructor(private router: Router, private userService: UserService, private postService: PostService) { 
     const navigation = this.router.getCurrentNavigation();
     console.log(navigation?.extras.state)
     const state = navigation?.extras.state as {
@@ -21,22 +23,26 @@ export class WallComponent implements OnInit {
     this.getUserByNickname(state.nickname);
   }
 
-
   ngOnInit(): void {
     this.getUsers();
+    this.getPosts();
   }
-
   getUserByNickname(nickname: string): void {
     this.userService.getUserByNickname(nickname)
       .subscribe(user => {
         this.user = user[0];
       })
   }
-
   getUsers(): void {
     this.userService.getUsers()
       .subscribe(users => {
         this.users = users;
+      })
+  }
+  getPosts(): void {
+    this.postService.getPosts()
+      .subscribe(posts => {
+        this.posts = posts;
       })
   }
 }
