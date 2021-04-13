217ca398387c63c663c2c2c62c3343fc28d92a3e
Ana Albuquerque <fc53512@alunos.fc.ul.pt>
2021-04-13 19:49:44 +0100

    login done - falta verificar user caracteres

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service'
 import { User } from '../user';
-import { Router,NavigationExtras } from '@angular/router'
+import { Router, NavigationExtras } from '@angular/router'
+import {FormControl, Validators} from '@angular/forms';
 
 
 @Component({
@@ -35,36 +36,20 @@ export class LoginComponent implements OnInit {
   }
 
   SignUp() {
-    let flag = true;
+    // let flag = this.userVerify();
+    // let flag2 = this.passVerify();
 
-    if(this.reg_username.length < 3){
-      alert("O username que escolheu tem de ter pelo menos 3 alagrismos");
-      flag = false;
-    }
-
-    if(this.reg_password.length < 8){
-      alert("A password tem de ter 8 ou mais caracteres");
-      flag = false;
-    }
-
-    for (let i = 0; i < this.users.length; i++) {
-      if (this.users[i].nickname == this.reg_username) {
-          alert("O username que escolheu ja está a ser utilizado");
-          flag = false;
-      }
-    }
-
-    if(flag){
+    if(this.userVerify() == true && this.passVerify() == true){
       this.user.password = this.reg_password;
       this.user.nickname = this.reg_username;
-      this.userService.addUser( {nickname: this.user.nickname, password: this.user.password} as User)
-      .subscribe(user => {
-        this.users.push(user);
-      });
-      
-      this.Router.navigate(['/wall'], {state: {nickname:this.user.nickname}});
-    }
+      this.userService.addUser({ nickname: this.user.nickname, password: this.user.password } as User)
+        .subscribe((user: User) => {
+          this.users.push(user);
+        });
 
+      this.Router.navigate(['/wall'], { state: { nickname: this.user.nickname } });
+    }
+    this.Router.navigate(['/login']);
   }
 
   login() {
@@ -74,18 +59,18 @@ export class LoginComponent implements OnInit {
       if (this.users[i].nickname == this.log_username) {
         if (this.users[i].password == this.log_password) {
           flag = true;
-          alert("O Utilizador autenticou com sucesso");
-          //Em caso de sucesso na autenticação o utilizador deve ser levado para um ecrã 
-          //que mostra as fotografias que carregou para a plataforma no passado, ou, se não tiver carregado nenhuma, 
+          alert("O Utilizador autenticou-se com sucesso.");
+          //Em caso de sucesso na autenticação o utilizador deve ser levado para um ecrã que mostra as 
+          //fotografias que carregou para a plataforma no passado, ou, se não tiver carregado nenhuma, 
           //para o ecrã com as fotografias mais recentes na plataforma
-          this.Router.navigate(['/wall'], {state: {nickname:this.user.nickname}});
+          this.Router.navigate(['/wall'], { state: { nickname: this.user.nickname } });
         }
       }
     }
 
     //Em caso de falha na autenticação, a mensagem de erro não deve permitir saber se o nickname existe no sistema
     if (!flag)
-      alert("Os dados do login estão incorretos. Por favor, tente novamente.");
+      alert("Os dados de login estão incorretos. Por favor, tente novamente.");
   }
 
   getUsers(): void {
@@ -95,4 +80,102 @@ export class LoginComponent implements OnInit {
       })
   }
 
+  passVerify(): boolean {
+    let flagLen = true;
+    let flagUper = false;
+    let flagN = false;
+    let flagLower = false;
+
+
+    if (this.reg_password.length < 8) {
+      flagLen = false;
+    }
+
+    for(let i = 0; i < this.reg_password.length; i++){
+      if( /[A-Z]/.test(this.reg_password.charAt(i))){
+        console.log("Uper");
+        flagUper = true;
+      }
+
+      if( /[a-z]/.test(this.reg_password.charAt(i))){
+        console.log("lowe");
+        flagLower = true;
+      }
+
+      if( /[0-9]/.test(this.reg_password.charAt(i))){
+        console.log("num");
+        flagN = true;
+      }
+    }
+
+    if(!flagLen){
+      alert("A password deve ter 8 ou mais carateres");
+      return false;
+    }
+
+    if(!flagLower){
+      alert("A password deve ter 1 ou mais caracteres minusculos");
+      return false;
+    }
+
+    if(!flagUper){
+      alert("A password deve ter 1 ou mais caracteres maiusculos");
+      return false;
+    }
+
+    if(!flagN){
+      alert("A password deve ter 1 ou mais caracteres algarismos");
+      return false;
+    }
+
+    // if(!flagLen || !flagLower || ! flagN || !flagUper){
+    //   return false;
+    // }
+    return true;
+  }
+
+  username = new FormControl("", [Validators.required]);
+
+  getErrorMessage() {
+    if (this.username.hasError('required')) {
+      return 'You must enter a value.';
+    }
+
+    return this.username.hasError('username') ? 'Not a valid username' : '';
+  }
+  password = new FormControl("", [
+    Validators.required
+    // Validators.pattern('^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{8,}$')
+  ]);
+
+  getErrorMessage2() {
+
+    if (this.password.hasError('required')) {
+      return 'You must enter a value.';
+    }
+    return this.password.hasError('password');
+  }
+
+  userVerify(): boolean {
+    let flag = true;
+
+    //O nickname será o nome pelo qual o utilizador é conhecido na plataforma e deve ser único e ter 3 
+    //ou mais carateres (só pode ter letras ou algarismos)
+    if (this.reg_username.length < 3) {
+      alert("O username tem de ter pelo menos 3 alagrismos");
+      return false;
+    }
+
+
+    for (let i = 0; i < this.users.length; i++) {
+      //Se o utilizador tentar registar um nickname que já existe, a mensagem de erro deve indicá-lo claramente
+      if (this.users[i].nickname == this.reg_username) {
+        alert("O username " + this.reg_username +  " já está a ser utilizado. Por favor escolha outro.");
+        return false;
+      }
+    }
+    return flag;
+  }
+
 }
+
